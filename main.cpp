19fe44be0f29b3c9e78c81f37d6023f349a4bf9c
Alice (SazonovPO) <alice@example.com>
2020-05-02 17:32:09 +0300

    code: aftermerge

--- main.cpp
+++ main.cpp
@@ -13,28 +13,18 @@ int main()
          << "A / B = " << a / b << '\n'
          if(b>a)
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
         cout<<b;
     }
     else
     {
         cout<<a;
-=======
-
-
-        cout<<a;
     }
-    else
+        if(b<a)
     {
-        cout<<b;
->>>>>>> 2ad13d18c533489b230710cbe7aae58c2d04177b
-=======
         cout<<b;
     }
     else
     {
         cout<<a;
->>>>>>> 63b3d873f2c8d5892b05428c967c180705889952
     }
 }
